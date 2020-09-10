304eac8134f797e425ff797b4deda2b63452fceb
Alex Wander <alexcwander@gmail.com>
2020-09-10 10:49:40 -0500

    Updated Balanced Bracket Solution

--- javascript/medium/balanced-bracket.js
+++ javascript/medium/balanced-bracket.js
@@ -1,36 +1,36 @@
 function isBalanced(s) {
-  // set default result variable
-  let result = "YES";
-  // declare empty array to push/pop characters for checks
-  let brackets = [];
+	// set default result variable
+	let result = 'YES';
+	// declare empty array to push/pop characters for checks
+	let brackets = [];
 
-  // split at every character and put into new array
-  let arr = s.split(""); // arr = [ '{', '[', '(', ')', ']', '}' ]
+	// split at every character and put into new array
+	let arr = s.split(''); // arr = [ '{', '[', '(', ')', ']', '}' ]
 
-  // loop through new array and check characters
-  arr.forEach((char) => {
-    if (char === "{") {
-      // push matching closing onto brackets array
-      brackets.push("}");
-    } else if (char === "[") {
-      // push matching closing onto brackets array
-      brackets.push("]");
-    } else if (char === "(") {
-      // push matching closing onto brackets array
-      brackets.push(")");
-      // if char is closing, check if it matches first element in brackets array
-    } else if (char !== brackets.pop()) {
-      result = "NO";
-    }
-  });
+	// loop through new array and check characters
+	arr.forEach((char) => {
+		if (char === '{') {
+			// push matching closing onto brackets array
+			brackets.push('}');
+		} else if (char === '[') {
+			// push matching closing onto brackets array
+			brackets.push(']');
+		} else if (char === '(') {
+			// push matching closing onto brackets array
+			brackets.push(')');
+			// if char is closing, check if it matches last element in brackets array
+		} else if (char !== brackets.pop()) {
+			result = 'NO';
+		}
+	});
 
-  // if any values left in the stack array, result is NO
-  if (brackets.length) {
-    result = "NO";
-  }
+	// if any values left in the brackets array, result is NO
+	if (brackets.length) {
+		result = 'NO';
+	}
 
-  return result;
+	return result;
 }
 
-console.log(isBalanced("{[()]}"));
-console.log(isBalanced("{[(])}"));
+console.log(isBalanced('{[()]}')); // YES
+console.log(isBalanced('{[(])}')); // NO
